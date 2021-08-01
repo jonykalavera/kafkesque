8b173a3a08f5423fbd58055055714a4b298eb504
vbazhin <v.bazhin@gmail.com>
2021-08-01 03:05:24 +0200

    Add dispatcher coroutine mockup

--- consumers/consumers.py
+++ consumers/consumers.py
@@ -5,10 +5,16 @@ from channels.consumer import AsyncConsumer
 import json
 from confluent_kafka import Consumer
 import httpx
+from redis import Redis
+# from .dispatchers import run_dispatcher
+import asyncio
+
 
 logger = logging.getLogger(__name__)
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', '192.168.178.25:9093')
+
+REDIS_CLIENT = Redis()
 
 class KafkaConsumerConsumer(AsyncConsumer):
     async def kafka_consume(self, config):
@@ -17,25 +23,46 @@ class KafkaConsumerConsumer(AsyncConsumer):
         auto_offset_reset = config.get('auto.offset.reset', 'earliest')
         topics = config['topics']
 
-        c = Consumer({
+        consumer = Consumer({
             'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS,
             'group.id': name,
             'auto.offset.reset': auto_offset_reset
         })
-        c.subscribe(topics)
+        consumer.subscribe(topics)
         print("Subscribed to:", topics)
-        while True:
-            msg = c.poll(0.1)
-            if msg is None:
-                continue
-            if msg.error():
-                print("Consumer error: {}".format(msg.error()))
-                continue
-            value = msg.value().decode('utf-8')
-            print('Received message: {}'.format(msg.value().decode('utf-8')))
-            if webhook:
-                httpx.post(webhook, data={
-                    # "key": msg.key().decode('utf-8'),
-                    "value": json.loads(value),
-                })
-        c.close()
+
+        # Run dispatcher
+        asyncio.run(run_dispatcher(topics))
+
+        # Run consumer
+        run_consumer(consumer, topics, webhook)
+
+
+async def run_dispatcher(topics):
+    while True:
+        msg = REDIS_CLIENT.rpop(topics)
+        if msg is None:
+            continue
+        httpx.post(
+            msg['url'],
+            data=msg['data']
+        )
+
+
+def run_consumer(consumer, topics, webhook):
+    while True:
+        msg = consumer.poll(0.1)
+        if msg is None:
+            continue
+        if msg.error():
+            print("Consumer error: {}".format(msg.error()))
+            continue
+        value = msg.value().decode('utf-8')
+        print('Received message: {}'.format(msg.value().decode('utf-8')))
+        if webhook:
+            # Push to Redis
+            REDIS_CLIENT.rpush(topics, {
+                'url': webhook,
+                'data': {'value': json.loads(value)}
+            })
+    consumer.close()
