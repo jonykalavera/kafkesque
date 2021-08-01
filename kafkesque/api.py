4fc11eeb0714c8cf086b7175af219bac9c4ffb77
Jony Kalavera <mr.jony@gmail.com>
2021-08-01 05:14:10 +0200

    rename api

--- kafkesque/api.py
+++ kafkesque/api.py
@@ -1,6 +1,6 @@
 from ninja import NinjaAPI
 from consumers.api import router as consumers_router
 
-api = NinjaAPI()
+api = NinjaAPI(title="Kafkesque")
 
 api.add_router("/consumers/", consumers_router)
